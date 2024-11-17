d88d2c93e5a8d8fe8bb87f59b359886dbd13589b
Felipe Macedo Gomes <104227174+femacedogomes@users.noreply.github.com>
2024-11-17 19:49:44 -0300

    feat: update layout portfolio pages

--- src/app/[locale]/portfolio/[slug]/page.tsx
+++ src/app/[locale]/portfolio/[slug]/page.tsx
@@ -35,7 +35,7 @@ const PortfolioPage = async ({ params }: { params: Params }) => {
           style="custom-height"
           content={(
             <div className="mx-auto flex flex-col justify-center gap-10 md:justify-start lg:mx-0">
-              <Text as="h1" styleOverride="h1" className="xs:text-sm text-center text-5xl font-light !leading-snug tracking-wider text-white lg:text-left">
+              <Text as="h1" styleOverride="h1" className=" text-center text-3xl font-normal !leading-snug tracking-wider text-white md:text-5xl md:font-light lg:text-left">
                 Veja como foi a
                 <br />
                 construção do site
@@ -51,47 +51,47 @@ const PortfolioPage = async ({ params }: { params: Params }) => {
           className="h-[300px] md:h-[60vh]"
         />
       </section>
-      <div className="container mt-32 text-white">
+      <div className="container mt-16 text-white md:mt-32">
 
-        <section className="grid grid-cols-11">
-          <Text as="h2" className="col-span-5 text-lg font-semibold">
+        <section className="flex flex-col gap-8 md:grid md:grid-cols-11">
+          <Text as="h5" className="col-span-5 font-semibold">
             INTRODUÇÃO
           </Text>
           <div className="col-span-6">
-            <Text as="h3" className="mb-5 text-3xl font-medium">
+            <Text as="h3" className="mb-5 text-3xl font-medium leading-snug">
               {selectedProject.introTitle}
             </Text>
-            {selectedProject.introDescription.map((text, index) => <p className="mb-3 tracking-wider" key={index}>{text}</p>)}
+            {selectedProject.introDescription.map((text, index) => <Text as="p" size="md" className="mt-3 tracking-wider" key={index}>{text}</Text>)}
           </div>
         </section>
 
         {/*  ESPAÇADOR */}
-        <div className="mx-auto my-16 h-[0.5px] w-[70vw] rounded-full bg-white opacity-10 blur-[0.5xp]  md:w-160"></div>
+        <div className="mx-auto my-20 h-[0.5px] w-[70vw] rounded-full bg-white opacity-10 blur-[0.5xp]  md:w-160"></div>
         {/*  ESPAÇADOR */}
 
-        <section className="grid grid-cols-11">
-          <Text as="h2" className="sticky top-20 col-span-5 h-max text-lg font-semibold">
+        <section className="flex flex-col gap-8 md:grid md:grid-cols-11">
+          <Text as="h5" className="col-span-5 h-max font-semibold md:sticky md:top-20">
             SITE ENTREGUE
           </Text>
           <Image src="/assets/images/portfolio/mr-advogados/image1.png" alt="mr-adv-image1" width={701} height={1590} className="col-span-6"></Image>
         </section>
 
         {/*  ESPAÇADOR */}
-        <div className="mx-auto my-16 h-[0.5px] w-[70vw] rounded-full bg-white opacity-10 blur-[0.5xp]  md:w-160"></div>
+        <div className="mx-auto my-20 h-[0.5px] w-[70vw] rounded-full bg-white opacity-10 blur-[0.5xp]  md:w-160"></div>
         {/*  ESPAÇADOR */}
 
-        <section className="relative grid grid-cols-11">
-          <div className="col-span-5 flex justify-start gap-10">
+        <section className="relative flex flex-col flex-wrap gap-8 lg:grid lg:grid-cols-11">
+          <div className="col-span-5 flex flex-wrap justify-center gap-10 md:flex-nowrap lg:justify-start">
             <Image src="/assets/images/portfolio/mr-advogados/image2.png" alt="mr-adv-image2" width={178} height={400} className="pt-10"></Image>
             <Image src="/assets/images/portfolio/mr-advogados/image3.png" alt="mr-adv-image3" width={178} height={400} className="pb-10"></Image>
           </div>
           <div className="col-span-6 ">
-            <Text as="h2" className="mb-3 text-lg font-bold tracking-wide"> DESENVOLVIMENTO</Text>
-            {selectedProject.developmentDescription}
-            <div className="mt-14 flex justify-between text-center">
+            <Text as="h5" className="mb-3 font-bold leading-snug tracking-wide"> DESENVOLVIMENTO</Text>
+            <Text as="p" size="md" className="tracking-wider">{selectedProject.developmentDescription}</Text>
+            <div className="mt-6 flex flex-wrap justify-around text-center sm:mt-14 md:justify-between">
               {selectedProject.metricAndValue.map((item, index) => (
                 <div key={index}>
-                  <Text as="p" className="text-5xl font-bold text-primary">
+                  <Text as="p" size="md" className="text-5xl font-bold text-primary">
                     {item.value}
                   </Text>
                   <Text as="p" className="pt-1">
@@ -104,23 +104,22 @@ const PortfolioPage = async ({ params }: { params: Params }) => {
         </section>
 
         {/*  ESPAÇADOR */}
-        <div className="mx-auto my-16 h-[0.5px] w-[70vw] rounded-full bg-white opacity-10 blur-[0.5xp]  md:w-160"></div>
+        <div className="mx-auto my-20 h-[0.5px] w-[70vw] rounded-full bg-white opacity-10 blur-[0.5xp]  md:w-160"></div>
         {/*  ESPAÇADOR */}
 
         <section>
-          <Text as="h2" className="sticky top-0 col-span-5 mb-4 text-lg font-semibold">
+          <Text as="h5" className="sticky top-0 col-span-5 mb-4 text-center font-semibold md:text-start">
             QUALIDADE E ENTREGA
           </Text>
-          <Text as="p" className="mb-8 max-w-[43%] text-base">
+          <Text as="p" size="md" className="mb-8 text-start lg:max-w-[44%]">
             {selectedProject.qualityAndDeliveryDescription}
           </Text>
-          <div className="relative grid grid-cols-2 tracking-wider">
+          <div className="relative flex grid-cols-2 flex-col gap-8 tracking-wider md:grid">
             <div className="flex flex-col items-start justify-between">
               {selectedProject.descriptions.map((description, index) => (
-                <div key={index} className="flex items-start gap-3">
-                  {' '}
-                  <Image src="/assets/images/icons/check.svg" alt="check-icon" width={22} height={22} className="opacity-20"></Image>
-                  {description}
+                <div key={index} className="flex items-start gap-4">
+                  <Image src="/assets/images/icons/check.svg" alt="check-icon" width={24} height={24} className="opacity-20"></Image>
+                  <Text as="p" size="md" className="tracking-wide">{description}</Text>
                 </div>
               ))}
             </div>
@@ -130,7 +129,7 @@ const PortfolioPage = async ({ params }: { params: Params }) => {
         </section>
 
         {/*  ESPAÇADOR */}
-        <div className="mx-auto my-16 h-[0.5px] w-[70vw] rounded-full bg-white opacity-10 blur-[0.5xp]  md:w-160"></div>
+        <div className="mx-auto my-20 h-[0.5px] w-[70vw] rounded-full bg-white opacity-10 blur-[0.5xp]  md:w-160"></div>
         {/*  ESPAÇADOR */}
       </div>
       <section>
@@ -139,10 +138,9 @@ const PortfolioPage = async ({ params }: { params: Params }) => {
           mediaSrc="/assets/images/tablet_working.png"
           style="custom-height"
           content={(
-            <div className="mx-auto flex flex-col items-center text-center">
-              <Text as="h1" styleOverride="h1" className="mx-auto text-3xl font-normal leading-loose tracking-normal text-white ">
+            <div className="mx-auto flex flex-col items-center justify-center text-center">
+              <Text as="h1" className="text-xl font-medium leading-loose tracking-normal text-white md:text-3xl md:font-normal">
                 MENTES
-                {' '}
                 <strong className="font-medium">CRIATIVAS</strong>
                 {' '}
                 E

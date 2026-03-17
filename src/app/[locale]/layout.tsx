c19d1daceac8e135f2c91f0788069c4440c58dbd
renan m <renanpontez@gmail.com>
2026-03-16 22:14:30 -0300

    perf: move Google Fonts from CSS @import to preconnected link tags

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -59,6 +59,18 @@ export default function RootLayout(props: {
 
   return (
     <html lang={props.params.locale}>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Sorts+Mill+Goudy:ital@0;1&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body>
         <NextIntlClientProvider
           locale={props.params.locale}
